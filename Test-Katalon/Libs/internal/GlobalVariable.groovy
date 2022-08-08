58f3cff0da1481eee06ec12b4243be8eb514a9e6
AdolBiondy <110831029+AdolBiondy@users.noreply.github.com>
2022-08-08 22:04:34 +0700

    Add files via upload
    Test Qoin1

---
+++ Test-Katalon/Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,42 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object BaseURL
+     
+    /**
+     * <p></p>
+     */
+    public static Object SuccessCode
+     
+    /**
+     * <p></p>
+     */
+    public static Object Email
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
+    
+            BaseURL = selectedVariables['BaseURL']
+            SuccessCode = selectedVariables['SuccessCode']
+            Email = selectedVariables['Email']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
---
+++ Test-Katalon/Test Suites/Testing Post with Get Single List User.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testing Post with Get Single List User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>873d18b7-e7d8-4c6f-a84c-bdebe121de09</testSuiteGuid>
+   <testCaseLink>
+      <guid>5f7d5d07-5be8-4b66-a8fe-7f99dcae68eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Get Single User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9dff4776-37b8-4017-a120-7d9d9e19ebaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Post</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
